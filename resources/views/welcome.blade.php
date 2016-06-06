5b990a51f79db7d3e3512a44ed14b82b8f900670
Tanya <tanyamiltsina@gmail.com>
2016-06-06 11:00:15 +0300

    registration page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,180 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
-
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <link href="../../css/style.css" rel="stylesheet">
+</head>
+<body class="main-page">
+      
+      <div class="check-in">
+                   <div class="form">
+                            <a class="logo" href=""></a>
+                            <h1 class="slogan">С теми, кто в теме</h1>  
+                    </div>
+                    <div class="preloader"></div>
         </div>
-    </body>
+
+<!-- REGISTRATION -->
+        <!-- <div class="check-in">
+               <div class="form">
+                        <a class="logo" href=""></a>
+                        <h1>С теми, кто в теме</h1>  
+
+                        <!--  first  step-- city & phone -->
+                        <!--<div class="form-item">
+                                    <h5>Регистрация</h5>
+                                    <form>
+                                        <div class="city-field">
+                                                <span class="city"></span>
+                                                <select>
+                                                        <option>Москва</option>
+                                                        <option>Москва</option>
+                                                        <option>Москва</option>
+                                                        <option>Москва</option>
+                                                        <option>Москва</option>
+                                                        <option>Москва</option>
+                                                        <option>Москва</option>
+                                                        <option>Москва</option>
+                                                </select>
+                                                <span class="city-inner"></span>
+                                        </div>
+                                        <div class="phone-field">
+                                        <span class="phone"></span>
+                                                    <input type="tel" name="" placeholder="Введите номер телефона...">
+                                        </div>
+                                        <p class="capcha-tel">Поле обязательно для заполнения</p>
+                                    </form>
+                                    <button class="check-in-button">Зарегистрироваться</button>
+                        </div> 
+
+                        <!--  second  step-- city & phone -->
+                       <!--  <div class="form-item">
+                                    <h5>Регистрация</h5>
+                                    <form>
+                                        <div class="code-field">
+                                            <span class="code"></span>
+                                                        <input type="text" name="" placeholder="Введите код...">
+                                            </div>
+                                            <p class="capcha-code">Поле обязательно для заполнения</p>
+                                    </form>
+                                    <button class="check-in-button">Зарегистрироваться</button>
+                        </div> -->
+                <!--</div>
+        </div> -->
+
+         <!--  third  step-- city & phone -->
+        <!-- <div class="check-in registration">
+               <div class="form">
+                        <a class="logo" href=""></a>
+                        <h1>С теми, кто в теме</h1>  
+                        <div class="form-item ">
+                                    <form>
+                                        <div class="add-ava"></div>
+                                        <div class="name-field">
+                                                    <span class="name"></span>
+                                                    <input type="text" name="" placeholder="Введите ваше имя...">
+                                        </div>
+                                        <p class="capcha-name">Поле обязательно для заполнения</p>
+                                        <div class="user-field">
+                                                    <span class="user"></span>
+                                                    <input type="text" name="" placeholder="Введите ваш логин...">
+                                        </div> 
+                                        <p class="capcha-user">Поле обязательно для заполнения</p>
+                                        <div class="sex-field">
+                                                <span class="sex"></span>
+                                                <select>
+                                                        <option>Мужской</option>
+                                                        <option>Женский</option>
+                                                </select>
+                                                <span class="sex-inner"></span>
+                                        </div>
+                                        <div class="password-field">
+                                                    <span class="password"></span>
+                                                    <input type="password" name="" placeholder="Введите ваш пароль...">
+                                        </div>
+                                        <p class="capcha-password">Поле обязательно для заполнения</p>
+                                    </form>
+                                    <button class="check-in-button">Готово</button>
+                        </div> 
+
+               </div>
+        </div> -->
+
+<!-- RECOVERY -->
+        <!-- <div class="check-in recovery">
+               <div class="form">
+                        <a class="logo" href=""></a>
+                        <h1>С теми, кто в теме</h1>  
+                        <button class="check-in-button">Войти</button>
+                        <a class="registration-button" href=""><span></span> Регистрация</a>
+               </div>
+        </div> -->
+
+        <!-- <div class="check-in recovery">
+               <div class="form">
+                        <a class="logo" href=""></a>
+                        <input class="text-field" type="text" name="" placeholder="Ваш номер телефона...">
+                        <p>На этот номер телефона мы oтправим вам SMS с кодом</p>
+                        <button class="check-in-button">Отправить код</button>
+                        <a class="registration-button enter-button" href="">Войти</a>
+               </div>
+        </div> -->
+
+        <!-- <div class="check-in recovery">
+               <div class="form">
+                        <a class="logo" href=""></a>
+                        <input class="text-field" type="text" name="" placeholder="Введите код...">
+                        <p>Введите код, полученный SMS сообщением на указанный номер телефона</p>
+                        <button class="check-in-button">Востановить</button>
+                        <a class="registration-button enter-button" href="">Войти</a>
+               </div>
+        </div> -->
+
+         <!-- <div class="check-in recovery">
+               <div class="form">
+                        <a class="logo" href=""></a>
+                        <input class="password-field-first" type="text" name="" placeholder="Придумайте новый пароль...">
+                        <input class="password-field-second" type="text" name="" placeholder="Повторите пароль...">
+                        <button class="check-in-button">Отправить</button>
+               </div>
+        </div>  -->
+
+         <!-- <div class="check-in recovery">
+               <div class="form">
+                        <a class="logo" href=""></a>
+                        <input class="password-field-first" type="text" name="" placeholder="Логин или номер телефона...">
+                        <input class="password-field-second" type="text" name="" placeholder="Пароль...">
+                        <button class="check-in-button">Войти</button>
+                        <div class="remember">
+                            <a href="">Забыли пароль? </a>
+                            <a href="">Регистрация</a>
+                        </div>
+               </div>
+        </div>  -->
+
+        <footer>
+                <div class="footer">
+                        <div class="footer-item">
+                                    <a href="">Place People <span>© 2016</span></a>
+                                    <div class="payment">
+                                                <a class="mastercard" href=""></a>
+                                                <a class="visa" href=""></a>
+                                    </div>
+                        </div>
+                        <div class="menu">
+                            <a class="drop-menu" href=""></a>
+                            <div class="menu-item">
+                                    <a href="">О сервисе </a>
+                                    <a href="">Помощь </a>
+                                    <a href="">Правила </a>
+                                    <a href="">Реклама </a>
+                                    <a href="">Разработчикам </a>
+                            </div>
+                        </div>
+                </div>
+        </footer>
+</body>
 </html>
