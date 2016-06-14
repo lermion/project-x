1e473d3a6067dab60c568ba875286268c2597c9a
Roman Golubenko <dneprogall@gmail.com>
2016-06-14 16:04:27 +0300

    [Front-end] Adaptive menu finished. Static pages rebuild. User authorisation check added.

--- public/app/Static/controllers/staticCtrl.js
+++ public/app/Static/controllers/staticCtrl.js
@@ -1,54 +1,71 @@
 angular.module('placePeopleApp')
-    .controller('staticCtrl', ['$scope', '$state', '$stateParams', 'StaticService', 'AuthService', '$window', '$http', 
-    	function($scope, $state, $stateParams, StaticService, AuthService, $window, $http){
+    .controller('staticCtrl', ['$scope', '$state', '$stateParams', 'StaticService', 'AuthService', '$window', '$http', 'storageService',
+    	function($scope, $state, $stateParams, StaticService, AuthService, $window, $http, storageService){
 
     	$scope.$emit('publicPoint', 'public');
 
+        var storage = storageService.getStorage();
+
+        if (storage.length) {
+            $scope.userLogged = true;
+            $scope.username = storage.username;
+        }else{
+            $scope.userLogged = false;
+        }
+
+        $scope.page = $stateParams.pageName;
+
     	$http.get('/static_page/get/name')
-            .success(function (response){            	
+            .success(function (response){                   	
                 $scope.staticPages = response;
             })
             .error(function (error){
                 console.log(error);
             });
 
-    	$scope.username = $window.sessionStorage.getItem('username');
-		$scope.page = $stateParams.pageName;
-
 		StaticService.getStatic($stateParams.pageName)
-			.then(function(res){					
-					$scope.staticTitle = res.title;							    			
-	    			$scope.staticText = res.text;
-	    			$scope.staticDesc = res.description;	    			
+			.then(function(res){			    			
+	    			$scope.staticText = res.text;	    			    			
 			      }, function(err){
 			        console.log(err);
 			      });
 		$scope.logOut = function(){
     		AuthService.userLogOut()
 	    		.then(function(res){
+                    storageService.deleteStorage();
 	    			$state.go('login');
 			      }, function(err){
 			        console.log(err);
 			      });
-    	} 
+    	}; 
+
+        $scope.openMenu = function(){
+            if ($window.innerWidth <= 800) {                
+                 $scope.showMenu =! $scope.showMenu;
+            } else{
+                $scope.showMenu = false;                
+            }
+        };
 
     	$scope.openBottomMenu = function(){
             if ($window.innerWidth <= 650) {
                 $scope.showBottomMenu = !$scope.showBottomMenu;
             } else {
                 $scope.showBottomMenu = false;
-            }    
-            // console.log('asd');        
+            }      
         };
 
-        var w = angular.element($window);    	
-    	var photosBlock = angular.element(document.querySelector('#user-page .my-photos'))[0];   	
-
+        var w = angular.element($window);
 		$scope.$watch(
 		  function () {
 		    return $window.innerWidth;
 		  },
-		  function (value) {			  
+		  function (value) {
+              if (value <= 800) {
+                $scope.showMenu = false;
+              } else {
+                $scope.showMenu = true;
+              }			  
 			  if (value <= 650) {
                 $scope.showBottomMenu = false;
               } else {
