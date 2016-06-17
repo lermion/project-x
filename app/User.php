22c89b4e2d3c4ac2cb8c3f894b9a5fe6ef075d8c
Nelipa Bogdan <nelipa.bogdan@gmail.com>
2016-06-17 14:50:10 +0300

    Added user status

--- app/User.php
+++ app/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'login', 'phone', 'password', 'country_id', 'first_name', 'last_name', 'gender'
+        'login', 'phone', 'password', 'country_id', 'first_name', 'last_name', 'gender', 'status'
     ];
 
     /**
