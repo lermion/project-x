29c6e29df71654b13e4fee0cef98a2aee1d60b9a
Tanya <tanyamiltsina@gmail.com>
2016-06-29 11:32:15 +0300

    -- user page fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <title>Place People</title>
-   <!--  <meta name="viewport" content="width=device-width"> -->
    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta name="description" content="">
     <link href="../../css/style.css" rel="stylesheet">
