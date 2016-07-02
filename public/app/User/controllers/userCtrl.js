70a25ec5678401daa3569c12cc39970b8016872c
unknown <lermion666@gmail.com>
2016-07-02 15:27:03 +0300

    fixed submit button in emoji smilies

--- public/app/User/controllers/userCtrl.js
+++ public/app/User/controllers/userCtrl.js
@@ -301,7 +301,7 @@ angular.module('placePeopleApp')
 		});
 		$scope.publishNewPub = function(files){
 			var pubText = angular.element(document.querySelector(".pubText")).val();
-			if (!pubText || files.length == 0) {						
+			if (!pubText || files === undefined || files.length == 0) {						
 				$scope.publishNewPubErr = true;				
 				return;				
 			}
