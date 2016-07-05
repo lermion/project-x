62d52b7fd0a9c9d5ef649e9f8464bd98a30f6065
unknown <lermion666@gmail.com>
2016-07-05 10:27:04 +0300

    fixed change users avatar

--- public/app/Settings/controllers/settingsCtrl.js
+++ public/app/Settings/controllers/settingsCtrl.js
@@ -1,60 +1,60 @@
 angular.module('placePeopleApp')
-    .controller('settingsCtrl', ['$scope', '$state', '$stateParams', 'StaticService', 'AuthService', 'UserService', '$window', '$http', 'storageService',  'ngDialog',
-    	function($scope, $state, $stateParams, StaticService, AuthService, UserService, $window, $http, storageService, ngDialog){
-    	$scope.$emit('userPoint', 'user');    	
+	.controller('settingsCtrl', ['$scope', '$state', '$stateParams', 'StaticService', 'AuthService', 'UserService', '$window', '$http', 'storageService',  'ngDialog',
+		function($scope, $state, $stateParams, StaticService, AuthService, UserService, $window, $http, storageService, ngDialog){
+		$scope.$emit('userPoint', 'user');    	
 		var storage = storageService.getStorage();
 		$scope.loggedUser = storage.username;
 
 		$http.get('/static_page/get/name')
-		            .success(function (response){            	
-		                $scope.staticPages = response;
-		            })
-		            .error(function (error){
-		                console.log(error);
-		            });
+					.success(function (response){            	
+						$scope.staticPages = response;
+					})
+					.error(function (error){
+						console.log(error);
+					});
 		$scope.logOut = function(){
-		    		AuthService.userLogOut()
-			    		.then(function(res){
-			    			storageService.deleteStorage();
-			    			$state.go('login');
-					      }, function(err){
-					        console.log(err);
-					      });
-		    	};
+					AuthService.userLogOut()
+						.then(function(res){
+							storageService.deleteStorage();
+							$state.go('login');
+						  }, function(err){
+							console.log(err);
+						  });
+				};
 
-    	$scope.openMenu = function(){
-    		if ($window.innerWidth <= 800) {    			
+		$scope.openMenu = function(){
+			if ($window.innerWidth <= 800) {    			
 				 $scope.showMenu =! $scope.showMenu;
-    		} else{
-    			$scope.showMenu = true;    			
-    		}
-    	};
+			} else{
+				$scope.showMenu = true;    			
+			}
+		};
 
-    	$scope.openBottomMenu = function(){
-            if ($window.innerWidth <= 650) {
-                $scope.showBottomMenu = !$scope.showBottomMenu;
-            } else {
-                $scope.showBottomMenu = false;
-            }
-        };
+		$scope.openBottomMenu = function(){
+			if ($window.innerWidth <= 650) {
+				$scope.showBottomMenu = !$scope.showBottomMenu;
+			} else {
+				$scope.showBottomMenu = false;
+			}
+		};
 
-        var w = angular.element($window);
+		var w = angular.element($window);
 		$scope.$watch(
 		  function () {
-		    return $window.innerWidth;
+			return $window.innerWidth;
 		  },
 		  function (value) {
 			  if (value <= 800) {
-			  	$scope.showMenu = false;
+				$scope.showMenu = false;
 			  } else {
-			  	$scope.showMenu = true;
+				$scope.showMenu = true;
 			  }
 
 			  if (value <= 650) {
-                $scope.showBottomMenu = false;
-              } else {
-                $scope.showBottomMenu = true;
-              }			  
+				$scope.showBottomMenu = false;
+			  } else {
+				$scope.showBottomMenu = true;
+			  }			  
 		  },
 		  true
 		);
@@ -64,11 +64,16 @@ angular.module('placePeopleApp')
 
 	/*Page code*/	
 
-	function blobToFile(theBlob, fileName){
-
-	    theBlob.lastModifiedDate = new Date();	    
-	    theBlob.filename = fileName;
-	    return theBlob;
+	function blobToFile(dataURI, callback){
+		var byteString = atob(dataURI.split(',')[1]);
+		var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+		var ab = new ArrayBuffer(byteString.length);
+		var ia = new Uint8Array(ab);
+		for (var i = 0; i < byteString.length; i++) {
+			ia[i] = byteString.charCodeAt(i);
+		}
+		var bb = new Blob([ab]);
+		return bb;
 	}
 
 	UserService.getUserData(storage.username)
@@ -167,13 +172,12 @@ angular.module('placePeopleApp')
 		ngDialog.closeAll();		
 		UserService.updateAvatar(blobFile)
 			.then(function(res){
-				console.log(res);
 				if (res.status) {
 					storageService.setStorageItem('loggedUserAva', res.user.avatar_path);
 				}				   			
-		      }, function(err){
-		        console.log(err);
-		      });	
+			  }, function(err){
+				console.log(err);
+			  });	
 	};
 
 }]);
