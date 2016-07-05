c34768fa933d4deba33a9618bb1dbb3f17658cfc
tryfonkov <trifonkov@gmail.com>
2016-07-05 15:14:41 +0300

    add places

---
+++ public/app/Places/controllers/places.ctrl.js
@@ -0,0 +1,23 @@
+(function (angular) {
+    'use strict';
+
+    angular
+        .module('app.places')
+        .controller('PlacesCtrl', PlacesCtrl);
+
+    PlacesCtrl.$inject = [];
+
+    function PlacesCtrl() {
+
+        var vm = this;
+
+        activate();
+
+        /////////////////////////////////////////////////
+
+        function activate() {
+
+        }
+    }
+
+})(angular);
