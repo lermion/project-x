08064f789edbb8adcd0a7b044856149d9ec91401
Artur Shishkin <shishikin.artur@gmail.com>
2016-07-07 12:17:27 +0300

    added dynamic places

--- app/TypePlace.php
+++ app/TypePlace.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class TypePlace extends Model
 {
     protected $fillable = [
-        'name','description','avatar'
+        'name','description','avatar','is_dynamic'
     ];
 }
