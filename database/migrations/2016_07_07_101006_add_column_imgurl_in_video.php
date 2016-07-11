28c7bda75e15c73e1512b0b6510e7b8bd169a71c
Roman Golubenko <dneprogall@gmail.com>
2016-07-11 16:27:34 +0300

    merge

---
+++ database/migrations/2016_07_07_101006_add_column_imgurl_in_video.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnImgurlInVideo extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('videos', function (Blueprint $table) {
+            $table->string('img_url');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('videos', function (Blueprint $table) {
+            //
+        });
+    }
+}
