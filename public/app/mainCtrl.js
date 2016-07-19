54352e22398cb190703e30a542ff13ec0859b659
tryfonkov <trifonkov@gmail.com>
2016-07-19 11:22:34 +0300

    added service for places, fix counter for newgroups, newplaces

--- public/app/mainCtrl.js
+++ public/app/mainCtrl.js
@@ -1,5 +1,5 @@
 angular.module('placePeopleApp')
-    .controller('mainCtrl', ['$rootScope', '$scope', 'groupsService', function ($rootScope, $scope, groupsService) {
+    .controller('mainCtrl', ['$rootScope', '$scope', 'groupsService', 'placesService', function ($rootScope, $scope, groupsService, placesService) {
 
         $scope.$on('publicPoint', function (event, data) {
             $scope.bodyClass = 'public';
@@ -17,6 +17,9 @@ angular.module('placePeopleApp')
             function (event, toState, toParams, fromState, fromParams) {
                 groupsService.getCounterNewGroups().then(function (data) {
                     $rootScope.counters.groupsNew = data;
+                });
+                placesService.getCounterNewPlaces().then(function (data) {
+                    $rootScope.counters.placessNew = data;
                 });
             })
     }]);
