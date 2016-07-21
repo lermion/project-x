ebce483bd6d3008435e95a2f626794a252a7dc27
tryfonkov <trifonkov@gmail.com>
2016-07-21 13:47:36 +0300

    add tabs for search results

--- public/app/Search/index.js
+++ public/app/Search/index.js
@@ -15,6 +15,22 @@
                 templateUrl: '../../app/Search/views/search.html',
                 controller: 'SearchCtrl',
                 controllerAs: 'vm'
+            })
+            .state('search.people', {
+                url: '/people',
+                templateUrl: '../../app/Search/views/search-people.html'
+            })
+            .state('search.publications', {
+                url: '/publications',
+                templateUrl: '../../app/Search/views/search-publications.html'
+            })
+            .state('search.places', {
+                url: '/places',
+                templateUrl: '../../app/Search/views/search-places.html'
+            })
+            .state('search.groups', {
+                url: '/groups',
+                templateUrl: '../../app/Search/views/search-groups.html'
             });
     }
 
