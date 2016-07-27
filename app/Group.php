e7a3ef495762db020948e0e782c68a835be286f0
Artur Shishkin <shishikin.artur@gmail.com>
2016-07-27 13:52:42 +0300

    Validation add places and groups

--- app/Group.php
+++ app/Group.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Group extends Model
 {
     protected $fillable = [
-        'name', 'url_name', 'description', 'is_open', 'avatar', 'card_avatar'
+        'name', 'url_name', 'description', 'is_open', 'avatar', 'card_avatar', 'room_id'
     ];
 
     public function users()
