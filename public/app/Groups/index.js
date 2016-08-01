d0d2a7177fa86c45b7e4a76ae55eb6257e2b5984
tryfonkov <trifonkov@gmail.com>
2016-08-01 14:34:37 +0300

    fix display full description of place

--- public/app/Groups/index.js
+++ public/app/Groups/index.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('app.groups', ['ngFileUpload', 'ngScrollbar'])
+        .module('app.groups', ['ngFileUpload', 'ngScrollbar', 'ngAnimate'])
         .config(routes);
 
 
