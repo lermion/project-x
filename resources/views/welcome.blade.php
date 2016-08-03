1998501b6a702886e944b3104ab702a144b6fc1b
unknown <lermion666@gmail.com>
2016-08-03 13:38:23 +0300

    fixed bug with header on auth page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
 	<link href="../../css/settings.css" rel="stylesheet">
 </head>
 <body ng-controller="mainCtrl" ng-class="bodyClass">
-	<header ng-cloak ng-hide="currentPath === '/auth/login' || currentPath === '/'">
+	<header ng-cloak ng-hide="currentPath === '/auth/login' || currentPath === '/' || currentPath === '/auth/registration'">
 		<div class="header header-user">
 			<a class="logo" ui-sref="feed"></a>
 			<div class="main-menu" ng-click="openMenu();" ng-class="showMenu ? 'show-menu' : ''" ng-init="showMenu=true;">
