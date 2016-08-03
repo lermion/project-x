7a67c2073d9b53ce388e56a9de64af882d052afa
tryfonkov <trifonkov@gmail.com>
2016-08-03 12:18:37 +0300

    fix search child state (prevent reload parent when search string change), fix display place/group in search result

--- public/app/common/directives/index.js
+++ public/app/common/directives/index.js
@@ -1,54 +1,73 @@
 (function (angular) {
 
-	'use strict';
+    'use strict';
 
-	angular
-		.module('app.directives', [])
-		.directive('validFile', validFile)
-		.directive('whenScrolled', whenScrolled, ["$timeout"])
-		.directive('scrollBottomOn', scrollBottomOn, ["$timeout"]);
+    angular
+        .module('app.directives', [])
+        .directive('validFile', validFile)
+        .directive('whenScrolled', whenScrolled, ["$timeout"])
+        .directive('scrollBottomOn', scrollBottomOn, ["$timeout"])
+        .directive('focusMe', focusMe, ['$timeout', '$parse']);
 
-	function validFile() {
-		return {
-			require: 'ngModel',
-			link: function (scope, el, attrs, ngModel) {
-				el.bind('change', function () {
-					scope.$apply(function () {
-						ngModel.$setViewValue(el.val());
-						ngModel.$render();
-					});
-				});
-			}
-		}
-	}
+    function validFile() {
+        return {
+            require: 'ngModel',
+            link: function (scope, el, attrs, ngModel) {
+                el.bind('change', function () {
+                    scope.$apply(function () {
+                        ngModel.$setViewValue(el.val());
+                        ngModel.$render();
+                    });
+                });
+            }
+        }
+    }
 
-	function whenScrolled() {
-		return function(scope, elm, attr) {
-			var raw = elm[0];
+    function whenScrolled() {
+        return function (scope, elm, attr) {
+            var raw = elm[0];
 
-			elm.bind('scroll', function() {
-				if (raw.scrollTop <= 100) {
-					var sh = raw.scrollHeight
-					scope.$apply(attr.whenScrolled).then(function() {
-						$timeout(function() {
-							raw.scrollTop = raw.scrollHeight - sh;
-						})
-					});
-				}
-			});
-		}
-	}
+            elm.bind('scroll', function () {
+                if (raw.scrollTop <= 100) {
+                    var sh = raw.scrollHeight
+                    scope.$apply(attr.whenScrolled).then(function () {
+                        $timeout(function () {
+                            raw.scrollTop = raw.scrollHeight - sh;
+                        })
+                    });
+                }
+            });
+        }
+    }
 
-	function scrollBottomOn() {
-		return function(scope, elm, attr) {
-			scope.$watch(attr.scrollBottomOn, function(value) {
-				if (value) {
-					$timeout(function() {
-						elm[0].scrollTop = elm[0].scrollHeight;
-					});
-				}
-			});
-		}
-	}
+    function scrollBottomOn() {
+        return function (scope, elm, attr) {
+            scope.$watch(attr.scrollBottomOn, function (value) {
+                if (value) {
+                    $timeout(function () {
+                        elm[0].scrollTop = elm[0].scrollHeight;
+                    });
+                }
+            });
+        }
+    }
+
+    function focusMe($timeout, $parse) {
+        return {
+            link: function(scope, element, attrs) {
+                var model = $parse(attrs.focusMe);
+                scope.$watch(model, function(value) {
+                    if(value === true) {
+                        $timeout(function() {
+                            element[0].focus();
+                        });
+                    }
+                });
+                //element.bind('blur', function() {
+                //    scope.$apply(model.assign(scope, false));
+                //});
+            }
+        };
+    }
 
 })(angular);
--- public/app/mainCtrl.js
+++ public/app/mainCtrl.js
@@ -1,65 +1,98 @@
 angular.module('placePeopleApp')
-	.controller('mainCtrl', ['$rootScope', '$scope', '$state', 'groupsService', 'placesService', 'storageService', 'AuthService', '$location', 'socket', function ($rootScope, $scope, $state, groupsService, placesService, storageService, AuthService, $location, socket) {
-		var storage = storageService.getStorage();
-		$scope.currentPath = $location.url();
-		$scope.loggedUserId = parseInt(storage.userId);
-		$scope.logOut = function(){
-			AuthService.userLogOut().then(function(response){
-				storageService.deleteStorage();
-				$state.go('login');
-			},
-			function(error){
-				console.log(error);
-			});
-		};
-		socket.emit("get user rooms", $scope.loggedUserId);
-		socket.on("get user rooms", function(response){
-			var chatRoomsArray = [];
-			for(var i = 0; i < response.length; i++){
-				if(response[i].countMessages > 0){
-					chatRoomsArray.push(response[i].countMessages);
-				}
-			}
-			$rootScope.countChatMessages = chatRoomsArray.length;
-		});
-		socket.on('updatechat', function(data){
-			socket.emit("get user rooms", $scope.loggedUserId);
-		});
-		$rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
-			var storage = storageService.getStorage();
-			$scope.loggedUser = storage.username;
-			$scope.currentPath = $location.url();
-		});
-		$scope.$on('publicPoint', function (event, data) {
-			$scope.bodyClass = 'public';
-		});
+    .controller('mainCtrl', ['$rootScope', '$scope', '$state', 'groupsService', 'placesService', 'storageService', 'AuthService', '$location', 'socket', function ($rootScope, $scope, $state, groupsService, placesService, storageService, AuthService, $location, socket) {
+        var storage = storageService.getStorage();
+        $scope.currentPath = $location.url();
+        $scope.loggedUserId = parseInt(storage.userId);
+        $scope.logOut = function () {
+            AuthService.userLogOut().then(function (response) {
+                    storageService.deleteStorage();
+                    $state.go('login');
+                },
+                function (error) {
+                    console.log(error);
+                });
+        };
+        socket.emit("get user rooms", $scope.loggedUserId);
+        socket.on("get user rooms", function (response) {
+            var chatRoomsArray = [];
+            for (var i = 0; i < response.length; i++) {
+                if (response[i].countMessages > 0) {
+                    chatRoomsArray.push(response[i].countMessages);
+                }
+            }
+            $rootScope.countChatMessages = chatRoomsArray.length;
+        });
+        socket.on('updatechat', function (data) {
+            socket.emit("get user rooms", $scope.loggedUserId);
+        });
+        $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+            var storage = storageService.getStorage();
+            $scope.loggedUser = storage.username;
+            $scope.currentPath = $location.url();
+        });
+        $scope.$on('publicPoint', function (event, data) {
+            $scope.bodyClass = 'public';
+        });
 
-		$scope.$on('authPoint', function (event, data) {
-			$scope.bodyClass = 'main-page';
-		});
+        $scope.$on('authPoint', function (event, data) {
+            $scope.bodyClass = 'main-page';
+        });
 
-		$scope.$on('userPoint', function (event, data) {
-			$scope.bodyClass = 'public user';
-		});
+        $scope.$on('userPoint', function (event, data) {
+            $scope.bodyClass = 'public user';
+        });
 
-		$rootScope.$on('$stateChangeStart',
-			function (event, toState, toParams, fromState, fromParams) {
-				groupsService.getCounterNewGroups().then(function (data) {
-					$rootScope.counters.groupsNew = data;
-				});
-				placesService.getCounterNewPlaces().then(function (data) {
-					$rootScope.counters.placessNew = data;
-				});
-			});
-		$scope.search = {
-			str: null,
-			byUsers: true,
-			byPublications: true,
-			byPlaces: true,
-			byGroups: true
-		};
-		$scope.submitSearch = function () {
+        $rootScope.$on('$stateChangeStart',
+            function (event, toState, toParams, fromState, fromParams) {
+                groupsService.getCounterNewGroups().then(function (data) {
+                    $rootScope.counters.groupsNew = data;
+                });
+                placesService.getCounterNewPlaces().then(function (data) {
+                    $rootScope.counters.placesNew = data;
+                });
 
-			$state.go('search', {'searchObj': $scope.search});
-		};
-	}]);
+                if (toState.name !== 'search' &&
+                    toState.name !== 'search.people' &&
+                    toState.name !== 'search.publications' &&
+                    toState.name !== 'search.places' &&
+                    toState.name !== 'search.groups') {
+
+                    resetSearch();
+
+                }
+
+            });
+
+
+        // Search
+        $scope.search = {
+            str: '',
+            byUsers: true,
+            byPublications: true,
+            byPlaces: true,
+            byGroups: true
+        };
+
+        var originalSearch = angular.copy($scope.search);
+
+        $scope.submitSearch = function () {
+            if ($state.current.name === 'search' ||
+                $state.current.name === 'search.people' ||
+                $state.current.name === 'search.publications' ||
+                $state.current.name === 'search.places' ||
+                $state.current.name === 'search.groups') {
+
+                $scope.$broadcast('search', {searchObj: angular.copy($scope.search)});
+
+            } else {
+                $state.go('search', {'searchObj': angular.copy($scope.search)});
+            }
+        };
+
+        function resetSearch() {
+            $scope.search = angular.copy(originalSearch);
+            $scope.showSearch = false;
+        }
+
+
+    }]);
