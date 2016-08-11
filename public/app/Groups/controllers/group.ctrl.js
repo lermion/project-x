45075f44b5e121ea054d1f6fd1727e5c5bf285ac
unknown <lermion666@gmail.com>
2016-08-11 17:21:23 +0300

    added directive for scroll to bottom in place and group chats

--- public/app/Groups/controllers/group.ctrl.js
+++ public/app/Groups/controllers/group.ctrl.js
@@ -1189,7 +1189,6 @@
         socket.emit("get group chat dialogue", getGroupChatDialogue);
         socket.on("get group chat dialogue", function (response) {
             $scope.messages = response.messages.reverse();
-            $scope.glued = true;
         });
         socket.on('updatechat', function (response) {
         	$scope.messages.push(response);
