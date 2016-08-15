8e012a31142f2db2cd16651919bfe98734ce2f49
tryfonkov <trifonkov@gmail.com>
2016-08-15 17:15:44 +0300

    fix geolocation for country select

--- public/app/Auth/controllers/authCtrl.js
+++ public/app/Auth/controllers/authCtrl.js
@@ -10,10 +10,23 @@ angular.module('placePeopleApp')
 
     	AuthService.getCountries()
     		.then(function(res){    			
-    			$scope.countries = res;		        
+    			$scope.countries = res;
 		      }, function(err){
 		        console.log(err);
 		      });
+			ymaps.ready(checkUserCountry);
+			function checkUserCountry() {
+				var geolocation = ymaps.geolocation;
+				geolocation.get({
+					provider: 'browser',
+					mapStateAutoApply: true
+				}).then(function (result) {
+					var metaData = result.geoObjects.get(0).properties.get('metaDataProperty');
+					var countryName = metaData.GeocoderMetaData.AddressDetails.Country.CountryName;
+					alert(countryName);
+
+				});
+			}
     	$scope.phoneRegExp = /[0-9]{3,18}/;
 
     	$http.get('/static_page/get/name')
