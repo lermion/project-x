cf765ccded78cbf4d9455eb4ad285eb80dc197dd
DimK <dmitriy@example.com>
2016-08-16 15:38:12 +0300

    edit admin - complains

--- resources/views/admin/complaints/index.blade.php
+++ resources/views/admin/complaints/index.blade.php
@@ -13,47 +13,137 @@
             <table class="table table-bordered">
                 <thead>
                   <tr>
-                    <th>id</th>
-                    <th>Текст</th>
-                    <th>Автор</th>
-                    <th>Картинки</th>
-                    <th>Кто пожаловался</th>
-                    <th>Жалобы</th>
-                    <th>Действия</th>
+                    <th class="col-id">id</th>
+                    <th class="col-text">Текст</th>
+                    <th class="col-author">Автор</th>
+                    <th class="col-img">Картинки</th>
+                    <th class="col-who">Кто пожаловался</th>
+                    <th class="col-complains">Жалобы</th>
+                    <th class="col-do">Действия</th>
                   </tr>
                 </thead>
                 <tbody>
                   <tr>
                     <td>1</td>
-                    <td>Публикация</td>
+                    <td>Публикация 123</td>
                     <td>Елена Новикова</td>
+                    <td class="mini-image">
+                        <img src="https://static.pexels.com/photos/20974/pexels-photo.jpg" alt="">
+                        <img src="https://static.pexels.com/photos/20974/pexels-photo.jpg" alt="">
+                        <img src="https://static.pexels.com/photos/20974/pexels-photo.jpg" alt="">
+                        <!-- <a href="">Ещё</a> -->
+                    </td>
                     <td>John Doe</td>
                     <td>Спам</td>
                     <td class="admin-user-contacts-action">
-                        <p><button class="btn btn-success btn-xs">Отменить жалобу</button></p>
-                        <p><button class="btn btn-danger btn-xs">Блокировать контент</button></p>
+                        <button class="btn btn-success btn-xs">Отменить жалобу</button>
+                        <button class="btn btn-danger btn-xs">Блокировать контент</button>
                     </td>
                   </tr>
                   <tr>
                     <td>1</td>
-                    <td>Коментарий</td>
+                    <td>Публикация 123</td>
                     <td>Елена Новикова</td>
+                    <td class="mini-image">
+                        <img src="https://static.pexels.com/photos/20974/pexels-photo.jpg" alt="">
+                        <img src="https://static.pexels.com/photos/20974/pexels-photo.jpg" alt="">
+                        <img src="https://static.pexels.com/photos/20974/pexels-photo.jpg" alt="">
+                        <!-- <a href="">Ещё</a> -->
+                    </td>
                     <td>John Doe</td>
-                    <td>Оскорбление</td>
+                    <td>Спам</td>
                     <td class="admin-user-contacts-action">
-                        <p><button class="btn btn-success btn-xs">Отменить жалобу</button></p>
-                        <p><button class="btn btn-danger btn-xs">Блокировать контент</button></p>
+                        <button class="btn btn-success btn-xs">Отменить жалобу</button>
+                        <button class="btn btn-danger btn-xs">Блокировать контент</button>
                     </td>
                   </tr>
                   <tr>
                     <td>1</td>
-                    <td>Публикация</td>
+                    <td>Публикация 123</td>
                     <td>Елена Новикова</td>
+                    <td class="mini-image">
+                        <img src="https://static.pexels.com/photos/20974/pexels-photo.jpg" alt="">
+                        <img src="https://static.pexels.com/photos/20974/pexels-photo.jpg" alt="">
+                        <img src="https://static.pexels.com/photos/20974/pexels-photo.jpg" alt="">
+                        <!-- <a href="">Ещё</a> -->
+                    </td>
                     <td>John Doe</td>
-                    <td>Материал для взрослых</td>
+                    <td>Спам</td>
                     <td class="admin-user-contacts-action">
-                        <p><button class="btn btn-success btn-xs">Отменить жалобу</button></p>
-                        <p><button class="btn btn-danger btn-xs">Блокировать контент</button></p>
+                        <button class="btn btn-success btn-xs">Отменить жалобу</button>
+                        <button class="btn btn-danger btn-xs">Блокировать контент</button>
+                    </td>
+                  </tr>
+                </tbody>
+            </table>
+        </div>
+
+
+        <br><br><br>
+
+        <div class="moderator-complaints">
+            <table class="table table-bordered">
+                <thead>
+                  <tr>
+                    <th class="col-id">id</th>
+                    <th class="col-text">Текст</th>
+                    <th class="col-author">Автор</th>
+                    <th class="col-img">Картинки</th>
+                    <th class="col-who">Кто пожаловался</th>
+                    <th class="col-complains">Жалобы</th>
+                    <th class="col-do">Действия</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>Коммент 123</td>
+                    <td>Елена Новикова</td>
+                    <td class="mini-image">
+                        <img src="https://static.pexels.com/photos/20974/pexels-photo.jpg" alt="">
+                        <img src="https://static.pexels.com/photos/20974/pexels-photo.jpg" alt="">
+                        <img src="https://static.pexels.com/photos/20974/pexels-photo.jpg" alt="">
+                        <!-- <a href="">Ещё</a> -->
+                    </td>
+                    <td>John Doe</td>
+                    <td>Спам</td>
+                    <td class="admin-user-contacts-action">
+                        <button class="btn btn-success btn-xs">Отменить жалобу</button>
+                        <button class="btn btn-danger btn-xs">Блокировать контент</button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>1</td>
+                    <td>Коммент 123</td>
+                    <td>Елена Новикова</td>
+                    <td class="mini-image">
+                        <img src="https://static.pexels.com/photos/20974/pexels-photo.jpg" alt="">
+                        <img src="https://static.pexels.com/photos/20974/pexels-photo.jpg" alt="">
+                        <img src="https://static.pexels.com/photos/20974/pexels-photo.jpg" alt="">
+                        <!-- <a href="">Ещё</a> -->
+                    </td>
+                    <td>John Doe</td>
+                    <td>Спам</td>
+                    <td class="admin-user-contacts-action">
+                        <button class="btn btn-success btn-xs">Отменить жалобу</button>
+                        <button class="btn btn-danger btn-xs">Блокировать контент</button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>1</td>
+                    <td>Коммент 123</td>
+                    <td>Елена Новикова</td>
+                    <td class="mini-image">
+                        <img src="https://static.pexels.com/photos/20974/pexels-photo.jpg" alt="">
+                        <img src="https://static.pexels.com/photos/20974/pexels-photo.jpg" alt="">
+                        <img src="https://static.pexels.com/photos/20974/pexels-photo.jpg" alt="">
+                        <!-- <a href="">Ещё</a> -->
+                    </td>
+                    <td>John Doe</td>
+                    <td>Спам</td>
+                    <td class="admin-user-contacts-action">
+                        <button class="btn btn-success btn-xs">Отменить жалобу</button>
+                        <button class="btn btn-danger btn-xs">Блокировать контент</button>
                     </td>
                   </tr>
                 </tbody>
