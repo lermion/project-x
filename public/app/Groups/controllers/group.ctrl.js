9fa45af53bccf56381dc7af886ba15c63c18c875
unknown <lermion666@gmail.com>
2016-08-17 12:25:54 +0300

    fixed index images in publication groups and places

--- public/app/Groups/controllers/group.ctrl.js
+++ public/app/Groups/controllers/group.ctrl.js
@@ -871,7 +871,8 @@
 
         };
 
-        vm.changeMainFile = function (file, flag, pub) {
+        vm.changeMainFile = function (file, flag, pub, index) {
+        	$scope.indexCurrentImage = index;
             if (file.pivot.video_id) {
                 vm.mainImage = "";
                 vm.mainVideo = file.url;
@@ -1258,17 +1259,6 @@
             });
         };
 
-        $scope.changeMainFile = function (file, flag, pub) {
-            if (flag) {
-                $scope.mainImageInPopup = file.url;
-            } else {
-                $scope.mainVideo = "";
-                $scope.mainImage = file.url;
-            }
-            if (flag === 'list') {
-                pub.mainFile = file;
-            }
-        };
         $scope.statusLoading = true;
         $scope.busyMessages = false;
         $scope.loadMoreMessages = function () {
