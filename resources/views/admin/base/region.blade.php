d3a35856011e7fa85c6d116c795c00d3e21dd7a7
DimK <dmitriy@example.com>
2016-08-17 03:33:35 +0300

    edit base

--- resources/views/admin/base/region.blade.php
+++ resources/views/admin/base/region.blade.php
@@ -1,1 +1,41 @@
 @extends('admin.layout')
+
+
+@section('content')
+
+    <div class="base">
+        <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th>id</th>
+                <th>Название</th>
+                <th>Страна</th>
+                <th>Действия</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <th>Днепропетровская область</th>
+                <td>Украина</td>
+                <td>
+                    <p class="text-center">
+                        <button class="btn btn-warning btn-xs">Редактировать</button>
+                    </p>
+                </td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <th>Московская область</th>
+                <td>Россия</td>
+                <td>
+                    <p class="text-center">
+                        <button class="btn btn-warning btn-xs">Редактировать</button>
+                    </p>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+    </div>
+
+@stop
