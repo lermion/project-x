727674957bc42167d60075d43745692e1283f370
unknown <lermion666@gmail.com>
2016-08-17 17:40:57 +0300

    added urls without hashtag

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -327,6 +327,8 @@ Route::group(['middleware' => ['web']], function () {
             </form>";
     });
 
-
-
+    Route::any( '{angularjs}', function ( $page ) {
+        return view('welcome');
+        
+    } )->where('angularjs', '(.*)');
 });
