b0df47205328e24d9bdcae61014b3bce5817eba1
DimK <dmitriy@example.com>
2016-08-17 10:11:40 +0300

    edit admin_settings - add tab

--- resources/views/admin/option/index.blade.php
+++ resources/views/admin/option/index.blade.php
@@ -2,61 +2,62 @@
 
 @section('content')
 
-	    <div class="x_content admin-settings">
+	    <div id="settings-tab" class="admin-settings">
 	        <ul class="row admin-settings-menu">
-	            <li class="col-md-3 active"><a href="">Общие настройки</a></li>
-	            <li class="col-md-3"><a href="">Ограничения</a></li>
+	            <li class="col-md-3"><a href="#settings-tab-common">Общие настройки</a></li>
+	            <li class="col-md-3"><a href="#settings-tab-limit">Ограничения</a></li>
 	        </ul>
+            <div id="settings-tab-common" class="admin-settings-data">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <label for="contacts">Контакты:</label>
+                            </div>
+                            <div class="col-md-7">
+                                <textarea class="form-control" rows="5" id="contacts">Контакты
+                                </textarea>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-4">
+                                <label for="сopyright">Copyright:</label>
+                            </div>
+                            <div class="col-md-7">
+                                <input class="form-control" id="сopyright" type="text" value="interfinity">
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-4">
+                                <label for="message-to-admin">Письмо админу:</label>
+                            </div>
+                            <div class="col-md-7">
+                                <input class="form-control" id="message-to-admin" type="text" value="xmugutdin@gmail.com">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div id="settings-tab-limit" class="admin-settings-limit">
+                <div class="row">
+                    <p>Пользователь за <input type="text" class="miniText form-control" value=""> мин. может написать не более <input type="text" class="miniText form-control" value=""> новым юзерам.</p>
+                    <div class="admin-settings-limit-toggle-btn"><span>Пользователь, не загрузивший свое фото, не может просматривать фото других пользователей</span>
+                        <div class="switch">
+                          <input id="cmn-toggle-1" class="cmn-toggle cmn-toggle-round" type="checkbox">
+                          <label for="cmn-toggle-1"></label>
+                        </div>
+                    </div>
+                    <p><input class="btn btn-primary" type="submit" value="Сохранить"></p>
+                </div>
+            </div>
 	    </div>
 
-        <form action="{{action('Admin\OptionController@create')}}" method="post" enctype="multipart/form-data">
-    <div class="x_content admin-settings-data">
-        <div class="row">
-            <div class="col-md-6">
-                <div class="row">
-                    <div class="col-md-3">
-                        <label for="contacts">Контакты:</label>
-                    </div>
-                    <div class="col-md-7">
-                        <textarea class="form-control" rows="5" id="contacts">{{$option->contacts}}
-                        </textarea>
-                    </div>
-                </div>
 
-                <div class="row">
-                    <div class="col-md-3">
-                        <label for="сopyright">Copyright:</label>
-                    </div>
-                    <div class="col-md-7">
-                        <input class="form-control" id="сopyright" type="text" value="{{$option->copyright}}">
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-md-3">
-                        <label for="message-to-admin">Письмо админу:</label>
-                    </div>
-                    <div class="col-md-7">
-                        <input class="form-control" id="message-to-admin" type="text" value="{{$option->mail}}">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-<br>
-
-    <div class="x_content admin-settings-limit">
-        <div class="row">
-            <p>Пользователь за <input type="text" class="miniText form-control" value="{{$option->users_chat_message}}"> мин. может написать не более <input type="text" class="miniText form-control" value="{{$option->time_chat_message}}"> новым юзерам.</p>
-            <div class="admin-settings-limit-toggle-btn"><span>Пользователь, не загрузивший свое фото, не может просматривать фото других пользователей</span>
-                <div class="switch">
-                  <input id="cmn-toggle-1" class="cmn-toggle cmn-toggle-round" type="checkbox">
-                  <label for="cmn-toggle-1"></label>
-                </div>
-            </div>
-            <p><input class="btn btn-primary" type="submit" value="Сохранить"></p>
-        </div>
-    </div>
-        </form>
+    <script>
+            $( "#settings-tab" ).tabs();
+    </script>
+        
 @stop
