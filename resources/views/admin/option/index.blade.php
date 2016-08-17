1534d306fe9650a4ebebce425206f98aa276c556
DimK <dmitriy@example.com>
2016-08-17 11:22:31 +0300

    edit admin - settings add form

--- resources/views/admin/option/index.blade.php
+++ resources/views/admin/option/index.blade.php
@@ -7,52 +7,55 @@
 	            <li class="col-md-3"><a href="#settings-tab-common">Общие настройки</a></li>
 	            <li class="col-md-3"><a href="#settings-tab-limit">Ограничения</a></li>
 	        </ul>
-            <div id="settings-tab-common" class="admin-settings-data">
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="row">
-                            <div class="col-md-4">
-                                <label for="contacts">Контакты:</label>
+            <form action="{{action('Admin\OptionController@create')}}" method="post">
+                <div id="settings-tab-common" class="admin-settings-data">
+                    <div class="row settings-tab-common-inner">
+                        <div class="col-md-6">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <label for="contacts">Контакты:</label>
+                                </div>
+                                <div class="col-md-7">
+                                    <textarea class="form-control" rows="5" id="contacts">Контакты
+                                    </textarea>
+                                </div>
                             </div>
-                            <div class="col-md-7">
-                                <textarea class="form-control" rows="5" id="contacts">Контакты
-                                </textarea>
-                            </div>
-                        </div>
 
-                        <div class="row">
-                            <div class="col-md-4">
-                                <label for="сopyright">Copyright:</label>
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <label for="сopyright">Copyright:</label>
+                                </div>
+                                <div class="col-md-7">
+                                    <input class="form-control" id="сopyright" type="text" value="interfinity">
+                                </div>
                             </div>
-                            <div class="col-md-7">
-                                <input class="form-control" id="сopyright" type="text" value="interfinity">
-                            </div>
-                        </div>
 
-                        <div class="row">
-                            <div class="col-md-4">
-                                <label for="message-to-admin">Письмо админу:</label>
-                            </div>
-                            <div class="col-md-7">
-                                <input class="form-control" id="message-to-admin" type="text" value="xmugutdin@gmail.com">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <label for="message-to-admin">Письмо админу:</label>
+                                </div>
+                                <div class="col-md-7">
+                                    <input class="form-control" id="message-to-admin" type="text" value="xmugutdin@gmail.com">
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div id="settings-tab-limit" class="admin-settings-limit">
-                <div class="row">
-                    <p>Пользователь за <input type="text" class="miniText form-control" value=""> мин. может написать не более <input type="text" class="miniText form-control" value=""> новым юзерам.</p>
-                    <div class="admin-settings-limit-toggle-btn"><span>Пользователь, не загрузивший свое фото, не может просматривать фото других пользователей</span>
-                        <div class="switch">
-                          <input id="cmn-toggle-1" class="cmn-toggle cmn-toggle-round" type="checkbox">
-                          <label for="cmn-toggle-1"></label>
+                <div id="settings-tab-limit" class="admin-settings-limit">
+                    <div class="row">
+                        <p>Пользователь за <input type="text" class="miniText form-control" value=""> мин. может написать не более <input type="text" class="miniText form-control" value=""> новым юзерам.</p>
+                        <div class="admin-settings-limit-toggle-btn"><span>Пользователь, не загрузивший свое фото, не может просматривать фото других пользователей</span>
+                            <div class="switch">
+                              <input id="cmn-toggle-1" class="cmn-toggle cmn-toggle-round" type="checkbox">
+                              <label for="cmn-toggle-1"></label>
+                            </div>
                         </div>
+                        
                     </div>
-                    <p><input class="btn btn-primary" type="submit" value="Сохранить"></p>
                 </div>
-            </div>
+                <p class="mg-l"><input class="btn btn-primary" type="submit" value="Сохранить"></p>
+            </form>
 	    </div>
 
 
