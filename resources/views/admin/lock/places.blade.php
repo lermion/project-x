ad4eeee971f226a722b150b7555fefe8a6cdc6ca
DimK <dmitriy@example.com>
2016-08-19 18:49:26 +0300

    edit admin_to_delete - link hover

--- resources/views/admin/lock/places.blade.php
+++ resources/views/admin/lock/places.blade.php
@@ -67,7 +67,17 @@
                 event.preventDefault();
                 return false;
             }
-        })
+        });
+        $('.admin-settings-menu li a').each(function () {
+            var location = window.location.href;
+            var link = this.href;
+            location += "/";
+            var index = location.indexOf(link);
+            console.log(index);
+            if(location == link) {
+                $(this).addClass('active');
+            }
+        });
     </script>
     {!! $places->render() !!}
 @stop
