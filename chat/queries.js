45859b86f84712f09f918f3cc0fd614589b7bad3
unknown <lermion666@gmail.com>
2016-08-25 16:11:39 +0300

    try to fix is read field

--- chat/queries.js
+++ chat/queries.js
@@ -213,7 +213,7 @@ Queries.prototype.getUserDialogue = function(data){
 				for(var i = 0; i < result.length; i++){
 					result[i].images = response[i];
 				}
-				connection.query("SELECT COUNT(message_id) FROM user_rooms_messages WHERE room_id = " + data.room_id + " AND message_id > (SELECT message_id FROM chat_notice_messages WHERE room_id = " + data.room_id + " AND NOT user_id = " + data.members[0] + ")", function(err, isRead) {
+				var sql = connection.query("SELECT COUNT(message_id) FROM user_rooms_messages WHERE room_id = " + data.room_id + " AND message_id > (SELECT message_id FROM chat_notice_messages WHERE room_id = " + data.room_id + " AND NOT user_id = " + data.members[0] + ")", function(err, isRead) {
 					console.log(isRead);
 					if(isRead !== undefined){
 						if(isRead[0]['COUNT(message_id)'] === 0){
@@ -231,6 +231,7 @@ Queries.prototype.getUserDialogue = function(data){
 					};
 					deferred.resolve(res);
 				});
+				console.log(sql);
 			});
 			var oldMessageArray = [];
 			result.forEach(function(value){
