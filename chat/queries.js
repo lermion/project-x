69167dfeb0f111cb16398613042b227e3bb43949
unknown <lermion666@gmail.com>
2016-08-25 16:09:01 +0300

    try to fix is read field

--- chat/queries.js
+++ chat/queries.js
@@ -214,6 +214,7 @@ Queries.prototype.getUserDialogue = function(data){
 					result[i].images = response[i];
 				}
 				connection.query("SELECT COUNT(message_id) FROM user_rooms_messages WHERE room_id = " + data.room_id + " AND message_id > (SELECT message_id FROM chat_notice_messages WHERE room_id = " + data.room_id + " AND NOT user_id = " + data.members[0] + ")", function(err, isRead) {
+					console.log(isRead);
 					if(isRead !== undefined){
 						if(isRead[0]['COUNT(message_id)'] === 0){
 						isRead = true;
