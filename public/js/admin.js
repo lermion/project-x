a0f220b3467805655fa4475900ed3c2306f32189
unknown <lermion666@gmail.com>
2016-08-26 11:22:06 +0300

    added get request for filter countries

--- public/js/admin.js
+++ public/js/admin.js
@@ -2,6 +2,10 @@ $("select.form-control").change(function(event){
 	$.get("get_region/" + parseInt(this.value), function(data){
 		if(data !== ""){
 			$("tbody.regions").html(data);
+			$("p.no-results").text("");
+		}else{
+			$("tbody.regions").html("");
+			$("p.no-results").text("нет результатов");
 		}
 	});
 });
