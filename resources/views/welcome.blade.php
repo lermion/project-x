3c1f47905fd9e1f2b27279ef84de0609d3079023
tryfonkov <trifonkov@gmail.com>
2016-08-26 16:39:44 +0300

    add Google Tag Manager

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,17 @@
 	<base href="/">
 </head>
 <body ng-controller="mainCtrl" ng-class="bodyClass">
+
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-KT9G9X"
+				  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KT9G9X');</script>
+<!-- End Google Tag Manager -->
+
 	<back-top ng-cloak class="main-up-button">Наверх</back-top>
 	<header ng-cloak ng-if="$root.isAuthorized && $root.showHeader && !(currentPath === '/auth/login' || currentPath === '/' || currentPath === '/auth/registration')">
 		<div class="header header-user">
