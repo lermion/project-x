6160911ee05b6a78088f9738412890d384adae8f
unknown <lermion666@gmail.com>
2016-08-29 16:31:28 +0300

    prepare to show graphics from date

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -26,67 +26,71 @@
 				keys: true
 			});
 			$('#datatable-responsive').DataTable();
-			var data1 = [
-				[gd(2012, 1, 1), 17],
-				[gd(2012, 1, 2), 74],
-				[gd(2012, 1, 3), 6],
-				[gd(2012, 1, 4), 39],
-				[gd(2012, 1, 5), 20],
-				[gd(2012, 1, 6), 85],
-				[gd(2012, 1, 7), 7]
-			];
-			$("#canvas_dahs").length && $.plot($("#canvas_dahs"), [data1], {
-				series: {
-					lines: {
-						show: false,
-						fill: true
-					},
-					splines: {
-						show: true,
-						tension: 0.4,
-						lineWidth: 1,
-						fill: 0.4
-					},
-					points: {
-						radius: 0,
-						show: true
-					},
-					shadowSize: 2
-				},
-				grid: {
-					verticalLines: true,
-					hoverable: true,
-					clickable: true,
-					tickColor: "#d5d5d5",
-					borderWidth: 1,
-					color: '#fff'
-				},
-				colors: ["rgba(38, 185, 154, 0.38)", "rgba(3, 88, 106, 0.38)"],
-				xaxis: {
-					tickColor: "rgba(51, 51, 51, 0.06)",
-					mode: "time",
-					tickSize: [1, "day"],
-					//tickLength: 10,
-					axisLabel: "Date",
-					axisLabelUseCanvas: true,
-					axisLabelFontSizePixels: 12,
-					axisLabelFontFamily: 'Verdana, Arial',
-					axisLabelPadding: 10
-					//mode: "time", timeformat: "%m/%d/%y", minTickSize: [1, "day"]
-				},
-				yaxis: {
-					ticks: 8,
-					tickColor: "rgba(51, 51, 51, 0.06)",
-				},
-				tooltip: false
-			});
 			function gd(year, month, day) {
 				return new Date(year, month - 1, day).getTime();
 			}
 			$("input#reservation").daterangepicker({}, function(start, end, label){
-				var url = "statistic/" + start.format('YYYY-MM-DD') + "/" + end.format('YYYY-MM-DD');
+				var url = "admin/statistic/" + start.format('YYYY-MM-DD') + "/" + end.format('YYYY-MM-DD');
 				$.get(url, function(response){
 					console.log(response);
+					response.forEach(function(value){
+						value
+					});
+					var data1 = [
+						[gd(2012, 1, 1), 17],
+						[gd(2012, 1, 2), 74],
+						[gd(2012, 1, 3), 6],
+						[gd(2012, 1, 4), 39],
+						[gd(2012, 1, 5), 20],
+						[gd(2012, 1, 6), 85],
+						[gd(2012, 1, 7), 7]
+					];
+					console.log(data1);
+					$("#canvas_dahs").length && $.plot($("#canvas_dahs"), [data1], {
+						series: {
+							lines: {
+								show: false,
+								fill: true
+							},
+							splines: {
+								show: true,
+								tension: 0.4,
+								lineWidth: 1,
+								fill: 0.4
+							},
+							points: {
+								radius: 0,
+								show: true
+							},
+							shadowSize: 2
+						},
+						grid: {
+							verticalLines: true,
+							hoverable: true,
+							clickable: true,
+							tickColor: "#d5d5d5",
+							borderWidth: 1,
+							color: '#fff'
+						},
+						colors: ["rgba(38, 185, 154, 0.38)", "rgba(3, 88, 106, 0.38)"],
+						xaxis: {
+							tickColor: "rgba(51, 51, 51, 0.06)",
+							mode: "time",
+							tickSize: [1, "day"],
+							//tickLength: 10,
+							axisLabel: "Date",
+							axisLabelUseCanvas: true,
+							axisLabelFontSizePixels: 12,
+							axisLabelFontFamily: 'Verdana, Arial',
+							axisLabelPadding: 10
+							//mode: "time", timeformat: "%m/%d/%y", minTickSize: [1, "day"]
+						},
+						yaxis: {
+							ticks: 8,
+							tickColor: "rgba(51, 51, 51, 0.06)",
+						},
+						tooltip: false
+					});
 				});
 			});
 		});
