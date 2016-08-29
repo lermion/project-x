2086c044eac15b27d50dc92c5ee522017a411e70
unknown <lermion666@gmail.com>
2016-08-29 17:32:11 +0300

    added default avatar for users in admin panel

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -50,7 +50,6 @@
 						{
 							"data": "avatar_path",
 							"render" : function(data, type, row){
-								console.log(data);
 								return data ? '<img src="' + data + '" />' : '<img src="/images/user.png" />';
 							}
 						},
@@ -108,7 +107,6 @@
 				var checkbox = $("input#avatar");
 				checkbox.val( checkbox[0].checked ? 1 : 0);
 				filters[1] = "&is_avatar=" + checkbox.val();
-				console.log(checkbox.val());
 				createUrl(filters);
 			});
 
@@ -250,7 +248,6 @@
 			var form = $('#deleteModal').find('form');
 
 			form.attr('action',form.attr('action')+'/'+$(this).attr('period'));
-			console.log($(this).attr('period'));
 			form.submit();
 			return false;
 		});
@@ -260,7 +257,6 @@
 			var link = this.href;
 			location += "/";
 			var index = location.indexOf(link);
-			console.log(index);
 			if(location == link) {
 				$(this).addClass('active');
 			}
