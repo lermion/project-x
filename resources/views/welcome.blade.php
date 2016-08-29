f293f32c30a5267bed41fcaae06a5e0cf92c089b
tryfonkov <trifonkov@gmail.com>
2016-08-29 09:41:26 +0300

    fix show mobile menu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
 			<img src="/images/loading-main.gif" alt="">
 		</div>
 	</div>
-	<div class="mobile-menu-overlay" ></div>
+	<div class="mobile-menu-overlay" ng-show="isOverlay"></div>
 	<!-- Scripts -->
 	<script type="text/javascript" src="../../app/libs/jquery-2.1.3.min.js"></script>
 	<script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
