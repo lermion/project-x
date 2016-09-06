15e26d95e5190e13f7e2391ae2e9908422e5cf0a
tryfonkov <trifonkov@gmail.com>
2016-09-06 17:05:09 +0300

    fix publications video, fix group event listener ngdialog

--- public/app/Groups/controllers/group.ctrl.js
+++ public/app/Groups/controllers/group.ctrl.js
@@ -196,28 +196,28 @@
 			vm.showPubSwitch = (state === 'place.publications' || state === 'group.publications');
 		});
 
-		//$scope.$on('ngDialog.opened', function (e, $dialog) {
-		//    if ($dialog.name === "modal-publication-group") {
-		//        var mainImagePublication = $(".main-image-publication");
-		//        setTimeout(function () {
-		//            mainImagePublication.focus();
-		//        }, 0);
-		//        $scope.keyPress = function (event, images) {
-		//            if (event.keyCode === 39) {
-		//                $scope.openNextInfo(images);
-		//            } else if (event.keyCode === 37) {
-		//                $scope.openPreviousInfo(images);
-		//            }
-		//        };
-		//    }
-		//    if ($dialog.name === "modal-edit-group") {
-		//        $(".ngdialog .emoji-wysiwyg-editor")[0].innerHTML = $filter('colonToSmiley')(vm.groupEdited.description);
-		//    }
-		//
-		//    if ($dialog.name === "modal-edit-publication") {
-		//        $(".ngdialog.user-publication-edit .emoji-wysiwyg-editor")[0].innerHTML = $filter('colonToSmiley')(vm.pubEdited.text);
-		//    }
-		//});
+		$scope.$on('ngDialog.opened', function (e, $dialog) {
+		    //if ($dialog.name === "modal-publication-group") {
+		    //    var mainImagePublication = $(".main-image-publication");
+		    //    setTimeout(function () {
+		    //        mainImagePublication.focus();
+		    //    }, 0);
+		    //    $scope.keyPress = function (event, images) {
+		    //        if (event.keyCode === 39) {
+		    //            $scope.openNextInfo(images);
+		    //        } else if (event.keyCode === 37) {
+		    //            $scope.openPreviousInfo(images);
+		    //        }
+		    //    };
+		    //}
+		    if ($dialog.name === "modal-edit-group") {
+		        $(".ngdialog .emoji-wysiwyg-editor")[0].innerHTML = $filter('colonToSmiley')(vm.groupEdited.description);
+		    }
+
+		    //if ($dialog.name === "modal-edit-publication") {
+		    //    $(".ngdialog.user-publication-edit .emoji-wysiwyg-editor")[0].innerHTML = $filter('colonToSmiley')(vm.pubEdited.text);
+		    //}
+		});
 
 
 		// Modal windows
