82e37a90ff9c3d51f7a70440c9a3fd07eb3c3147
unknown <lermion666@gmail.com>
2016-09-08 10:20:29 +0300

    added logic for close/open registration

--- public/app/Auth/models/authService.js
+++ public/app/Auth/models/authService.js
@@ -12,149 +12,162 @@ angular.module('placePeopleApp')
 			userLogOut: userLogOut,
 			sendRestoreSms: sendRestoreSms,
 			validateRestoreSms: validateRestoreSms,
-			changePwd: changePwd
+			changePwd: changePwd,
+			isClosedRegistration: isClosedRegistration
 		}
 
 		function getCountries(){                
-            var defer = $q.defer();
-                $http.get(path + 'country/')
-                    .success(function (response){
-                        defer.resolve(response);
-                    })
-                    .error(function (error){
-                        defer.reject(error);
-                    });
-            return defer.promise;
-        }
+			var defer = $q.defer();
+				$http.get(path + 'country/')
+					.success(function (response){
+						defer.resolve(response);
+					})
+					.error(function (error){
+						defer.reject(error);
+					});
+			return defer.promise;
+		}
 
-        function sendMessage(phone, countryId){
-    		var data = {
-    			phone: phone,
-    			country_id: countryId
-    		};             
-            var defer = $q.defer();
-                $http.post(path + 'auth/create', data)
-                    .success(function (response){
-                        defer.resolve(response);
-                    })
-                    .error(function (error){
-                        defer.reject(error);
-                    });
-            return defer.promise;
-        } 
+		function isClosedRegistration(){
+			var defer = $q.defer();
+				$http.get("auth/closed_registration")
+					.success(function (response){
+						defer.resolve(response);
+					})
+					.error(function (error){
+						defer.reject(error);
+					});
+			return defer.promise;
+		}
 
-        function checkSms(code){
-    		var data = {
-    			sms_code: code
-    		};             
-            var defer = $q.defer();
-                $http.post(path + 'auth/check_sms', data)
-                    .success(function (response){
-                        defer.resolve(response);
-                    })
-                    .error(function (error){
-                        defer.reject(error);
-                    });
-            return defer.promise;
-           }
+		function sendMessage(phone, countryId){
+			var data = {
+				phone: phone,
+				country_id: countryId
+			};             
+			var defer = $q.defer();
+				$http.post(path + 'auth/create', data)
+					.success(function (response){
+						defer.resolve(response);
+					})
+					.error(function (error){
+						defer.reject(error);
+					});
+			return defer.promise;
+		} 
 
-        function registerUser(first, last, gender, login, pwd, countryId, avatar, userId, originalAvatar){           
-            var data = new FormData();
-            data.append('first_name', first);
-            data.append('last_name', last);
-            data.append('gender', gender);
-            data.append('login', login);
-            data.append('password', pwd);
-            data.append('country_id', countryId);
-            data.append('avatar', avatar);            
-            data.append('original_avatar', originalAvatar);            
-            var config = {
-                    headers: {
-                        'Content-Type': undefined
-                    },
-                    transformRequest: angular.identity
-                },            
-                defer = $q.defer();
-                    $http.post(path + 'user/add_first_info', data, config)
-	                    .success(function (response){
-	                        defer.resolve(response);
-	                    })
-	                    .error(function (error){
-	                        defer.reject(error);
-	                    });
-                return defer.promise;
-        	}
+		function checkSms(code){
+			var data = {
+				sms_code: code
+			};             
+			var defer = $q.defer();
+				$http.post(path + 'auth/check_sms', data)
+					.success(function (response){
+						defer.resolve(response);
+					})
+					.error(function (error){
+						defer.reject(error);
+					});
+			return defer.promise;
+		   }
 
-        function userLogIn(login, pwd){        	
+		function registerUser(first, last, gender, login, pwd, countryId, avatar, userId, originalAvatar){           
+			var data = new FormData();
+			data.append('first_name', first);
+			data.append('last_name', last);
+			data.append('gender', gender);
+			data.append('login', login);
+			data.append('password', pwd);
+			data.append('country_id', countryId);
+			data.append('avatar', avatar);            
+			data.append('original_avatar', originalAvatar);            
+			var config = {
+					headers: {
+						'Content-Type': undefined
+					},
+					transformRequest: angular.identity
+				},            
+				defer = $q.defer();
+					$http.post(path + 'user/add_first_info', data, config)
+						.success(function (response){
+							defer.resolve(response);
+						})
+						.error(function (error){
+							defer.reject(error);
+						});
+				return defer.promise;
+			}
+
+		function userLogIn(login, pwd){        	
 			var data = {        			
-        			login: login,
-        			password: pwd
-        		};             
-            var defer = $q.defer();
-                    $http.post(path + 'auth', data)
-	                    .success(function (response){
-	                        defer.resolve(response);
-	                    })
-	                    .error(function (error){
-	                        defer.reject(error);
-	                    });
-                return defer.promise;
-        }
+					login: login,
+					password: pwd
+				};             
+			var defer = $q.defer();
+					$http.post(path + 'auth', data)
+						.success(function (response){
+							defer.resolve(response);
+						})
+						.error(function (error){
+							defer.reject(error);
+						});
+				return defer.promise;
+		}
 
-        function userLogOut(login, pwd){
-            var defer = $q.defer();
-                    $http.get(path + 'auth/log_out')
-	                    .success(function (response){
-	                        defer.resolve(response);
-	                    })
-	                    .error(function (error){
-	                        defer.reject(error);
-	                    });
-                return defer.promise;
-        }
+		function userLogOut(login, pwd){
+			var defer = $q.defer();
+					$http.get(path + 'auth/log_out')
+						.success(function (response){
+							defer.resolve(response);
+						})
+						.error(function (error){
+							defer.reject(error);
+						});
+				return defer.promise;
+		}
 
-        function sendRestoreSms(phone){        	
+		function sendRestoreSms(phone){        	
 			var data = {        			
-        			phone: phone
-        		};             
-            var defer = $q.defer();
-                    $http.post(path + 'password/restore', data)
-	                    .success(function (response){
-	                        defer.resolve(response);
-	                    })
-	                    .error(function (error){
-	                        defer.reject(error);
-	                    });
-                return defer.promise;
-        }
+					phone: phone
+				};             
+			var defer = $q.defer();
+					$http.post(path + 'password/restore', data)
+						.success(function (response){
+							defer.resolve(response);
+						})
+						.error(function (error){
+							defer.reject(error);
+						});
+				return defer.promise;
+		}
 
-        function validateRestoreSms(code){        	
+		function validateRestoreSms(code){        	
 			var data = {        			
-        			sms_code: code
-        		};             
-            var defer = $q.defer();
-                    $http.post(path + 'password/validate_code', data)
-	                    .success(function (response){
-	                        defer.resolve(response);
-	                    })
-	                    .error(function (error){
-	                        defer.reject(error);
-	                    });
-                return defer.promise;
-        }
+					sms_code: code
+				};             
+			var defer = $q.defer();
+					$http.post(path + 'password/validate_code', data)
+						.success(function (response){
+							defer.resolve(response);
+						})
+						.error(function (error){
+							defer.reject(error);
+						});
+				return defer.promise;
+		}
 
-        function changePwd(pwd){        	
+		function changePwd(pwd){        	
 			var data = {        			
-        			password: pwd        			
-        		};             
-            var defer = $q.defer();
-                    $http.post(path + 'password/amend', data)
-	                    .success(function (response){
-	                        defer.resolve(response);
-	                    })
-	                    .error(function (error){
-	                        defer.reject(error);
-	                    });
-                return defer.promise;
-        }
+					password: pwd        			
+				};             
+			var defer = $q.defer();
+					$http.post(path + 'password/amend', data)
+						.success(function (response){
+							defer.resolve(response);
+						})
+						.error(function (error){
+							defer.reject(error);
+						});
+				return defer.promise;
+		}
 	}]);
