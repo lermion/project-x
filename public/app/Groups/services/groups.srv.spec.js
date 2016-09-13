b6b5f88ffd5eef64687a9cb87ced3b3477086c73
unknown <lermion666@gmail.com>
2016-09-13 10:23:12 +0300

    added first jasmine test

--- public/app/Groups/services/groups.srv.spec.js
+++ public/app/Groups/services/groups.srv.spec.js
@@ -1,12 +1,12 @@
 describe('Groups factory', function () {
     var Users;
-
+    beforeEach(module('ui.router'));
     // Before each test load our api.users module
     beforeEach(angular.mock.module('app.groups'));
 
 
     // Before each test set our injected Users factory (_Users_) to our local Users variable
-    beforeEach(inject(function (_groupsService_) {
+    beforeEach(inject(function(_groupsService_){
         groupsService = _groupsService_;
     }));
     //
