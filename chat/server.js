f46e71dbb4c59d2a63b0ee5fbecb1389b746864f
unknown <lermion666@gmail.com>
2016-09-13 16:41:57 +0300

    added ssl to serverjs

--- chat/server.js
+++ chat/server.js
@@ -1,6 +1,6 @@
 var app = require('express')();
 var fs = require('fs');
-var data = fs.readFileSync('./config.json');
+var data = fs.readFileSync('./config.json', 'utf8');
 var config = JSON.parse(data);
 var options = {
    key  : config.privateKey,
