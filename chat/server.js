f4e1a8e32b247ac3bd7e3b958f85353e3437f04e
unknown <lermion666@gmail.com>
2016-09-13 17:15:43 +0300

    try to add ssl to serverjs

--- chat/server.js
+++ chat/server.js
@@ -3,8 +3,8 @@ var fs = require('fs');
 var data = fs.readFileSync('./config.json');
 var config = JSON.parse(data);
 var options = {
-   key  : fs.readFileSync('./server.enc.key'),
-   cert : fs.readFileSync('./server.enc.crt')
+   key  : fs.readFileSync('./server.key'),
+   cert : fs.readFileSync('./server.csr')
 };
 var server = require('https').Server(options, app);
 var io = require('socket.io')(server);
