34560695ab1fa00678ed577ea03caa0effc508a9
unknown <lermion666@gmail.com>
2016-09-16 13:07:28 +0300

    changed title of site to LudiGor

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html lang="en" ng-app="placePeopleApp">
 <head>
 	<meta charset="UTF-8">
-	<title>Place People</title>
+	<title>Ludi Gor</title>
    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 	<meta name="description" content="">
 	<link href="../../css/style.css" rel="stylesheet">
