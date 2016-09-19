dd5b490a49f05fb5e42f04c02aaf0c2d406362de
DimK <dmitriy@example.com>
2016-09-19 16:55:45 +0300

    admin - title

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -6,7 +6,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Place People. Панель управления.</title>
+	<title>Ludi Gor. Панель управления.</title>
 
 	<!-- Bootstrap core CSS -->
 
