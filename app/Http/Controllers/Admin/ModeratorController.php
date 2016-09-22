6123968aba71b759b425d910b1f9366be7c83cad
Artur Shishkin <shishkin.artur@gmail.com>
2016-09-22 16:59:51 +0300

    add scope serch

--- app/Http/Controllers/Admin/ModeratorController.php
+++ app/Http/Controllers/Admin/ModeratorController.php
@@ -234,168 +234,174 @@ class ModeratorController extends Controller
         $moderator->email = $data['email'];
         $from_time = $data['from_time'];
         $to_time = $data['to_time'];
-        $weekday = $data['weekday'];
-        if (isset ($weekday[1]) && $weekday[1]=='on'){
-            WorkingHoursModerator::where(['weekday'=>1,'moderator_id'=>$moderator['id']])->delete();
-            $from_times = explode(":", $from_time[1]);
-            $hour = $from_times[0];
-            $min = $from_times[1];
-            $working_time = Carbon::create(null, null, null, $hour, $min, 0, NULL)->timestamp;
-            $to_times = explode(":", $to_time[1]);
-            $to_hour = $to_times[0];
-            $to_min = $to_times[1];
-            $to_working_time = Carbon::create(null, null, null, $to_hour, $to_min, 0, NULL)->timestamp;
-            if($working_time<$to_working_time) {
-                $working_hours = WorkingHoursModerator::where('weekday',1)->get();
-                $valid = $this->valid($working_hours,$to_working_time,$working_time);
-                if ($valid == 0){
-                    WorkingHoursModerator::create(
-                        ['weekday'=>1,'from_time'=>$from_time[1],
-                            'to_time'=>$to_time[1],
-                            'moderator_id'=>$moderator['id']
-                        ]);
+        if(isset($data['weekday'])) {
+            $weekday = $data['weekday'];
+        }
+        try {
+            if (isset ($weekday[1]) && $weekday[1] == 'on') {
+                WorkingHoursModerator::where(['weekday' => 1, 'moderator_id' => $moderator['id']])->delete();
+                $from_times = explode(":", $from_time[1]);
+                $hour = $from_times[0];
+                $min = $from_times[1];
+                $working_time = Carbon::create(null, null, null, $hour, $min, 0, NULL)->timestamp;
+                $to_times = explode(":", $to_time[1]);
+                $to_hour = $to_times[0];
+                $to_min = $to_times[1];
+                $to_working_time = Carbon::create(null, null, null, $to_hour, $to_min, 0, NULL)->timestamp;
+                if ($working_time < $to_working_time) {
+                    $working_hours = WorkingHoursModerator::where('weekday', 1)->get();
+                    $valid = $this->valid($working_hours, $to_working_time, $working_time);
+                    if ($valid == 0) {
+                        WorkingHoursModerator::create(
+                            ['weekday' => 1, 'from_time' => $from_time[1],
+                                'to_time' => $to_time[1],
+                                'moderator_id' => $moderator['id']
+                            ]);
+                    }
                 }
-            }
-        };
+            };
 
-        if (isset ($weekday[2]) && $weekday[2]=='on'){
-            WorkingHoursModerator::where(['weekday'=>2,'moderator_id'=>$moderator['id']])->delete();
-            $from_times = explode(":", $from_time[2]);
-            $hour = $from_times[0];
-            $min = $from_times[1];
-            $working_time = Carbon::create(null, null, null, $hour, $min, 0, NULL)->timestamp;
-            $to_times = explode(":", $to_time[2]);
-            $to_hour = $to_times[0];
-            $to_min = $to_times[1];
-            $to_working_time = Carbon::create(null, null, null, $to_hour, $to_min, 0, NULL)->timestamp;
-            if($working_time<$to_working_time) {
-                $working_hours = WorkingHoursModerator::where('weekday',2)->get();
-                $valid = $this->valid($working_hours,$to_working_time,$working_time);
-                if ($valid == 0){
-                    WorkingHoursModerator::create(
-                        ['weekday'=>2,'from_time'=>$from_time[2],
-                            'to_time'=>$to_time[2],
-                            'moderator_id'=>$moderator['id']
-                        ]);
+            if (isset ($weekday[2]) && $weekday[2] == 'on') {
+                WorkingHoursModerator::where(['weekday' => 2, 'moderator_id' => $moderator['id']])->delete();
+                $from_times = explode(":", $from_time[2]);
+                $hour = $from_times[0];
+                $min = $from_times[1];
+                $working_time = Carbon::create(null, null, null, $hour, $min, 0, NULL)->timestamp;
+                $to_times = explode(":", $to_time[2]);
+                $to_hour = $to_times[0];
+                $to_min = $to_times[1];
+                $to_working_time = Carbon::create(null, null, null, $to_hour, $to_min, 0, NULL)->timestamp;
+                if ($working_time < $to_working_time) {
+                    $working_hours = WorkingHoursModerator::where('weekday', 2)->get();
+                    $valid = $this->valid($working_hours, $to_working_time, $working_time);
+                    if ($valid == 0) {
+                        WorkingHoursModerator::create(
+                            ['weekday' => 2, 'from_time' => $from_time[2],
+                                'to_time' => $to_time[2],
+                                'moderator_id' => $moderator['id']
+                            ]);
+                    }
                 }
-            }
-        };
+            };
 
-        if (isset ($weekday[3]) && $weekday[3]=='on'){
-            WorkingHoursModerator::where(['weekday'=>3,'moderator_id'=>$moderator['id']])->delete();
-            $from_times = explode(":", $from_time[3]);
-            $hour = $from_times[0];
-            $min = $from_times[1];
-            $working_time = Carbon::create(null, null, null, $hour, $min, 0, NULL)->timestamp;
-            $to_times = explode(":", $to_time[3]);
-            $to_hour = $to_times[0];
-            $to_min = $to_times[1];
-            $to_working_time = Carbon::create(null, null, null, $to_hour, $to_min, 0, NULL)->timestamp;
-            if($working_time<$to_working_time) {
-                $working_hours = WorkingHoursModerator::where('weekday',3)->get();
-                $valid = $this->valid($working_hours,$to_working_time,$working_time);
-                if ($valid == 0){
-                    WorkingHoursModerator::create(
-                        ['weekday'=>3,'from_time'=>$from_time[3],
-                            'to_time'=>$to_time[3],
-                            'moderator_id'=>$moderator['id']
-                        ]);
+            if (isset ($weekday[3]) && $weekday[3] == 'on') {
+                WorkingHoursModerator::where(['weekday' => 3, 'moderator_id' => $moderator['id']])->delete();
+                $from_times = explode(":", $from_time[3]);
+                $hour = $from_times[0];
+                $min = $from_times[1];
+                $working_time = Carbon::create(null, null, null, $hour, $min, 0, NULL)->timestamp;
+                $to_times = explode(":", $to_time[3]);
+                $to_hour = $to_times[0];
+                $to_min = $to_times[1];
+                $to_working_time = Carbon::create(null, null, null, $to_hour, $to_min, 0, NULL)->timestamp;
+                if ($working_time < $to_working_time) {
+                    $working_hours = WorkingHoursModerator::where('weekday', 3)->get();
+                    $valid = $this->valid($working_hours, $to_working_time, $working_time);
+                    if ($valid == 0) {
+                        WorkingHoursModerator::create(
+                            ['weekday' => 3, 'from_time' => $from_time[3],
+                                'to_time' => $to_time[3],
+                                'moderator_id' => $moderator['id']
+                            ]);
+                    }
                 }
-            }
-        };
+            };
 
-        if (isset ($weekday[4]) && $weekday[4]=='on'){
-            WorkingHoursModerator::where(['weekday'=>4,'moderator_id'=>$moderator['id']])->delete();
-            $from_times = explode(":", $from_time[4]);
-            $hour = $from_times[0];
-            $min = $from_times[1];
-            $working_time = Carbon::create(null, null, null, $hour, $min, 0, NULL)->timestamp;
-            $to_times = explode(":", $to_time[4]);
-            $to_hour = $to_times[0];
-            $to_min = $to_times[1];
-            $to_working_time = Carbon::create(null, null, null, $to_hour, $to_min, 0, NULL)->timestamp;
-            if($working_time<$to_working_time) {
-                $working_hours = WorkingHoursModerator::where('weekday',4)->get();
-                $valid = $this->valid($working_hours,$to_working_time,$working_time);
-                if ($valid == 0){
-                    WorkingHoursModerator::create(
-                        ['weekday'=>4,'from_time'=>$from_time[4],
-                            'to_time'=>$to_time[4],
-                            'moderator_id'=>$moderator['id']
-                        ]);
+            if (isset ($weekday[4]) && $weekday[4] == 'on') {
+                WorkingHoursModerator::where(['weekday' => 4, 'moderator_id' => $moderator['id']])->delete();
+                $from_times = explode(":", $from_time[4]);
+                $hour = $from_times[0];
+                $min = $from_times[1];
+                $working_time = Carbon::create(null, null, null, $hour, $min, 0, NULL)->timestamp;
+                $to_times = explode(":", $to_time[4]);
+                $to_hour = $to_times[0];
+                $to_min = $to_times[1];
+                $to_working_time = Carbon::create(null, null, null, $to_hour, $to_min, 0, NULL)->timestamp;
+                if ($working_time < $to_working_time) {
+                    $working_hours = WorkingHoursModerator::where('weekday', 4)->get();
+                    $valid = $this->valid($working_hours, $to_working_time, $working_time);
+                    if ($valid == 0) {
+                        WorkingHoursModerator::create(
+                            ['weekday' => 4, 'from_time' => $from_time[4],
+                                'to_time' => $to_time[4],
+                                'moderator_id' => $moderator['id']
+                            ]);
+                    }
                 }
-            }
-        };
+            };
 
-        if (isset ($weekday[5]) && $weekday[5]=='on'){
-            WorkingHoursModerator::where(['weekday'=>5,'moderator_id'=>$moderator['id']])->delete();
-            $from_times = explode(":", $from_time[5]);
-            $hour = $from_times[0];
-            $min = $from_times[1];
-            $working_time = Carbon::create(null, null, null, $hour, $min, 0, NULL)->timestamp;
-            $to_times = explode(":", $to_time[5]);
-            $to_hour = $to_times[0];
-            $to_min = $to_times[1];
-            $to_working_time = Carbon::create(null, null, null, $to_hour, $to_min, 0, NULL)->timestamp;
-            if($working_time<$to_working_time) {
-                $working_hours = WorkingHoursModerator::where('weekday',5)->get();
-                $valid = $this->valid($working_hours,$to_working_time,$working_time);
-                if ($valid == 0){
+            if (isset ($weekday[5]) && $weekday[5] == 'on') {
+                WorkingHoursModerator::where(['weekday' => 5, 'moderator_id' => $moderator['id']])->delete();
+                $from_times = explode(":", $from_time[5]);
+                $hour = $from_times[0];
+                $min = $from_times[1];
+                $working_time = Carbon::create(null, null, null, $hour, $min, 0, NULL)->timestamp;
+                $to_times = explode(":", $to_time[5]);
+                $to_hour = $to_times[0];
+                $to_min = $to_times[1];
+                $to_working_time = Carbon::create(null, null, null, $to_hour, $to_min, 0, NULL)->timestamp;
+                if ($working_time < $to_working_time) {
+                    $working_hours = WorkingHoursModerator::where('weekday', 5)->get();
+                    $valid = $this->valid($working_hours, $to_working_time, $working_time);
+                    if ($valid == 0) {
                         WorkingHoursModerator::create(
                             ['weekday' => 5, 'from_time' => $from_time[5],
                                 'to_time' => $to_time[5],
                                 'moderator_id' => $moderator['id']
                             ]);
+                    }
                 }
-            }
-        };
+            };
 
-        if (isset ($weekday[6]) && $weekday[6]=='on'){
-            WorkingHoursModerator::where(['weekday'=>6,'moderator_id'=>$moderator['id']])->delete();
-            $from_times = explode(":", $from_time[6]);
-            $hour = $from_times[0];
-            $min = $from_times[1];
-            $working_time = Carbon::create(null, null, null, $hour, $min, 0, NULL)->timestamp;
-            $to_times = explode(":", $to_time[6]);
-            $to_hour = $to_times[0];
-            $to_min = $to_times[1];
-            $to_working_time = Carbon::create(null, null, null, $to_hour, $to_min, 0, NULL)->timestamp;
-            if($working_time<$to_working_time) {
-                $working_hours = WorkingHoursModerator::where('weekday',6)->get();
-                $valid = $this->valid($working_hours,$to_working_time,$working_time);
-                if ($valid == 0){
-                    WorkingHoursModerator::create(
-                        ['weekday'=>6,'from_time'=>$from_time[6],
-                            'to_time'=>$to_time[6],
-                            'moderator_id'=>$moderator['id']
-                        ]);
+            if (isset ($weekday[6]) && $weekday[6] == 'on') {
+                WorkingHoursModerator::where(['weekday' => 6, 'moderator_id' => $moderator['id']])->delete();
+                $from_times = explode(":", $from_time[6]);
+                $hour = $from_times[0];
+                $min = $from_times[1];
+                $working_time = Carbon::create(null, null, null, $hour, $min, 0, NULL)->timestamp;
+                $to_times = explode(":", $to_time[6]);
+                $to_hour = $to_times[0];
+                $to_min = $to_times[1];
+                $to_working_time = Carbon::create(null, null, null, $to_hour, $to_min, 0, NULL)->timestamp;
+                if ($working_time < $to_working_time) {
+                    $working_hours = WorkingHoursModerator::where('weekday', 6)->get();
+                    $valid = $this->valid($working_hours, $to_working_time, $working_time);
+                    if ($valid == 0) {
+                        WorkingHoursModerator::create(
+                            ['weekday' => 6, 'from_time' => $from_time[6],
+                                'to_time' => $to_time[6],
+                                'moderator_id' => $moderator['id']
+                            ]);
+                    }
                 }
-            }
-        };
+            };
 
-        if (isset ($weekday[0]) && $weekday[0]=='on'){
-            WorkingHoursModerator::where(['weekday'=>0,'moderator_id'=>$moderator['id']])->delete();
-            $from_times = explode(":", $from_time[0]);
-            $hour = $from_times[0];
-            $min = $from_times[1];
-            $working_time = Carbon::create(null, null, null, $hour, $min, 0, NULL)->timestamp;
-            $to_times = explode(":", $to_time[0]);
-            $to_hour = $to_times[0];
-            $to_min = $to_times[1];
-            $to_working_time = Carbon::create(null, null, null, $to_hour, $to_min, 0, NULL)->timestamp;
-            if($working_time<$to_working_time) {
-                $working_hours = WorkingHoursModerator::where('weekday',0)->get();
-                $valid = $this->valid($working_hours,$to_working_time,$working_time);
-                if ($valid == 0){
-                    WorkingHoursModerator::create(
-                        ['weekday'=>0,'from_time'=>$from_time[0],
-                            'to_time'=>$to_time[0],
-                            'moderator_id'=>$moderator['id']
-                        ]);
+            if (isset ($weekday[0]) && $weekday[0] == 'on') {
+                WorkingHoursModerator::where(['weekday' => 0, 'moderator_id' => $moderator['id']])->delete();
+                $from_times = explode(":", $from_time[0]);
+                $hour = $from_times[0];
+                $min = $from_times[1];
+                $working_time = Carbon::create(null, null, null, $hour, $min, 0, NULL)->timestamp;
+                $to_times = explode(":", $to_time[0]);
+                $to_hour = $to_times[0];
+                $to_min = $to_times[1];
+                $to_working_time = Carbon::create(null, null, null, $to_hour, $to_min, 0, NULL)->timestamp;
+                if ($working_time < $to_working_time) {
+                    $working_hours = WorkingHoursModerator::where('weekday', 0)->get();
+                    $valid = $this->valid($working_hours, $to_working_time, $working_time);
+                    if ($valid == 0) {
+                        WorkingHoursModerator::create(
+                            ['weekday' => 0, 'from_time' => $from_time[0],
+                                'to_time' => $to_time[0],
+                                'moderator_id' => $moderator['id']
+                            ]);
+                    }
                 }
-            }
-        };
-
+            };
+        }
+        catch (\Exception $ex) {
+            return redirect()->back()->with('message', 'Модератор не изменен');
+        }
         $moderator->save();
 
         //$moderators['working_hours'] = WorkingHoursModerator::where('moderator_id',$id);
