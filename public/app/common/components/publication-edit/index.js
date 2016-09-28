f00f40df6a89838a5442043d8a739f90b0bcb1b3
unknown <lermion666@gmail.com>
2016-09-28 17:00:50 +0300

    fixed bug with crop area for vertical images

--- public/app/common/components/publication-edit/index.js
+++ public/app/common/components/publication-edit/index.js
@@ -227,6 +227,17 @@
 					if(isNewFile){
 							var reader = new FileReader();
 							reader.onload = function(event){
+								var image = new Image();
+								image.src = event.target.result;
+								image.onload = function(){
+									if(this.height > this.width){
+										ctrl.aspectRatio = 1.4;
+									}else if(this.width === 1366 && this.height === 768){
+										ctrl.aspectRatio = 2.5;
+									}else{
+										ctrl.aspectRatio = 1.7;
+									}
+								};
 								$scope.$apply(function($scope){
 									ctrl.coverToCrop = event.target.result;
 									ctrl.coverToCropName = ctrl.newFiles[index].name;
@@ -239,6 +250,17 @@
 							blob.name = ctrl.files[index].original_img_url.split("original_images/")[1];
 							var reader = new FileReader();
 							reader.onload = function(event){
+								var image = new Image();
+								image.src = event.target.result;
+								image.onload = function(){
+									if(this.height > this.width){
+										ctrl.aspectRatio = 1.4;
+									}else if(this.width === 1366 && this.height === 768){
+										ctrl.aspectRatio = 2.5;
+									}else{
+										ctrl.aspectRatio = 1.7;
+									}
+								};
 								$scope.$apply(function($scope){
 									ctrl.coverToCrop = event.target.result;
 									ctrl.coverToCropName = blob.name;
