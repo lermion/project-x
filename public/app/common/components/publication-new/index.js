26d5f837325c20422277f716e01052aacb536c89
unknown <lermion666@gmail.com>
2016-09-30 10:39:33 +0300

    fixed showing error messages after open popup create publication

--- public/app/common/components/publication-new/index.js
+++ public/app/common/components/publication-new/index.js
@@ -133,7 +133,9 @@
 										if(this.height > this.width){
 											ctrl.aspectRatio = 1.4;
 										}else{
-											this.height -= 200;
+											var heightPresent = this.height / 100;
+											heightPresent *= 20;
+											this.height = this.height - heightPresent;
 											ctrl.aspectRatio = this.width / this.height;
 										}
 										$scope.$apply(function($scope){
@@ -199,7 +201,9 @@
 								if(this.height > this.width){
 									ctrl.aspectRatio = 1.4;
 								}else{
-									this.height -= 200;
+									var heightPresent = this.height / 100;
+									heightPresent *= 20;
+									this.height = this.height - heightPresent;
 									ctrl.aspectRatio = this.width / this.height;
 								}
 								$scope.$apply(function($scope){
