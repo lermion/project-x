89220939ef79b1ff9bec4299d2e6978398db2769
Nikolay Golub <flittymac@MacBook-Pro-Nikolay.local>
2016-11-24 09:09:44 +0200

    mail

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'ludi.gor.msc@gmail.com', 'name' => 'Admin Ludi Gor'],
 
     /*
     |--------------------------------------------------------------------------
